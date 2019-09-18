81a1d09cc639ae46a12ac51497cb498ed171a6cd
AndroidProjects <payalvarade@gmail.com>
2019-09-18 16:18:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tablelayout/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tablelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.editText);
+        ratingBar=findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                String s1= String.valueOf(editText.getText());
+                Float f=ratingBar.getRating();
+                Toast.makeText(MainActivity.this, "Movie Nmae"+s1+"Rating:"+f, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
